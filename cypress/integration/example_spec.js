066f1dda7610c7d3cbbd4e26eaf06ec165f8c18a
Erica <erica.gucciardo@updater.com>
2018-11-01 15:08:46 -0400

    update based on jens comments

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,56 +1,29 @@
-const json = require('../fixtures/example.json');
-
-function setBaseUrl() {
-  Cypress.config('baseUrl', 'http://localhost:8080');
-}
-
 describe('Local Storage Reproducer', () => {
-  let value;
   beforeEach(() => {
-    setBaseUrl();
     // comment out below for tests to now be passing!
     cy.request({
       method: 'POST',
       url: 'http://nlv4d.mocklab.io/json2',
+    }).should((resp) => {
+      expect(resp.status).to.eq(200)
     })
     cy.visit('/', {
       onBeforeLoad: (win) => {
-        value = win.localStorage.getItem('redux_test');
-        console.log('redux_test', value);
+        expect(win.localStorage.getItem('redux_test')).to.be.null;
       }
     });
     cy.get('#test');
   })
   context('testing', () => {
     it('1', () => {
-      expect(value).to.be.null;
+      cy.window().then((win) => {
+        expect(win.localStorage.getItem('redux_test')).to.be.object;
+      })
     })
     it('2', () => {
-      expect(value).to.be.null;
-    })
-    it('3', () => {
-      expect(value).to.be.null;
-    })
-    it('4', () => {
-      expect(value).to.be.null;
-    })
-    it('5', () => {
-      expect(value).to.be.null;
-    })
-    it('6', () => {
-      expect(value).to.be.null;
-    })
-    it('7', () => {
-      expect(value).to.be.null;
-    })
-    it('8', () => {
-      expect(value).to.be.null;
-    })
-    it('9', () => {
-      expect(value).to.be.null;
-    })
-    it('10', () => {
-      expect(value).to.be.null;
+      cy.window().then((win) => {
+        expect(win.localStorage.getItem('redux_test')).to.be.object;
+      })
     })
   })
 });
